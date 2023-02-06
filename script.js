ce4609aa36299cf5308d8368000f40e7c7b8e154
Nick Sheward <nickshwrd@gmail.com>
2023-02-06 12:07:33 +0000

    Converted .info method return to an array and used this to structure the output for library cards

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function Book(title, author, pages, haveRead) {
   this.pages = pages;
   this.haveRead = haveRead;
   this.info = function () {
-    return`${title} ${author} ${pages} ${haveRead}`;
+    return [title, author, pages, haveRead];
   };
 }
 
@@ -35,11 +35,22 @@ function displayLibrary () {
   removeDivs();
   myLibrary.forEach(function (item) {
     let libraryBook = document.createElement('div');
+    let libraryBookTitle = document.createElement('div');
+    let libraryBookAuthor = document.createElement('div');
+    let libraryBookPages = document.createElement('div');
+    let libraryBookHaveRead = document.createElement('div');
     libraryBook.className = 'myLibraryCards';
     libraryBookValue = item.info();
-    libraryBook.innerText = libraryBookValue
+    libraryBookTitle.innerText = "Title: " +libraryBookValue[0]
+    libraryBookAuthor.innerText = "Author: " +libraryBookValue[1]
+    libraryBookPages.innerText = "Number of pages: " +libraryBookValue[2]
+    libraryBookHaveRead.innerText = "Read: " +libraryBookValue[3]
     libraryBook.style.backgroundColor = "grey";
     libraryContainer.appendChild(libraryBook);
+    libraryBook.appendChild(libraryBookTitle);
+    libraryBook.appendChild(libraryBookAuthor);
+    libraryBook.appendChild(libraryBookPages);
+    libraryBook.appendChild(libraryBookHaveRead);
   });
 }
 

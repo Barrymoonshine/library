cb12bdc3ccac7754a1a729578dd2972347b6dedf
Nick Sheward <nickshwrd@gmail.com>
2023-02-06 09:31:06 +0000

    Created reset book function and stopped page from reloading when form submitted

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # library
 Library project from the Odin Project 
+
+- Objects can't be printed onto a page using DOM methods, print a string onto the page instead,investigate info method 
+- Investigate re-rendering, and why displayLibrary() is re-rendering the myLibrary array
+-
--- script.js
+++ script.js
@@ -18,10 +18,27 @@ function addBookToLibrary() {
   let newBook = new Book(titleValue, authorValue, pagesValue, haveReadValue);
   myLibrary.push(newBook);
   displayLibrary();
+  resetAddBookForm();
 }
 
 function displayLibrary () {
-  myLibrary.forEach(function (item,index) {
-    console.log(item,index);
+  myLibrary.forEach(function (item) {
+    let libraryBook = document.createElement('div');
+    libraryBook.className = 'myLibrary';
+    libraryBookValue = item.info();
+    libraryBook.innerText = libraryBookValue
+    libraryBook.style.backgroundColor = "grey";
+    document.getElementById('libraryContainer').appendChild(libraryBook);
   });
 }
+
+function resetAddBookForm () {
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('pages').value = "";
+  document.getElementById('haveRead').checked = false;
+}
+
+document.getElementById('submitButton').addEventListener("click", (e) => {
+  addBookToLibrary();
+});

1845ae1ae0d6f674ee69f4a1a555d3c2f96a0873
Nick Sheward <nickshwrd@gmail.com>
2023-02-08 14:19:57 +0000

    Created removeModal function

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Library project from the Odin Project
 
 - Objects can't be printed onto a page using DOM methods, print a string onto the page instead,investigate info method 
 - Investigate re-rendering, and why displayLibrary() is re-rendering the myLibrary array
--
+- Check box on, change on to have read 
+
+
+Learnings 
+- default button type in a form is submit
+- Objects can't be added to the page using DOM methods 
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const modal = document.getElementById('newBookModal');
 const modalButton = document.getElementById('modalButton');
 const span = document.getElementsByClassName("close")[0];
 const submitButton = document.getElementById('submitButton');
+const body = document.getElementById('body');
 let myLibrary = [];
 
 submitButton.addEventListener("click", () => {
@@ -10,17 +11,14 @@ submitButton.addEventListener("click", () => {
 });
 modalButton.addEventListener("click", () => {
   modal.style.display ="block";
+  body.style.backgroundColor = "rgba (0,0,0,0.4)";
+  
 });
 span.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// When the user clicks anywhere outside of the modal, close it 
-window.onclick= function (e) {
-  if(e.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
 
 function Book(title, author, pages, haveRead) {
   this.title = title;
@@ -49,6 +47,7 @@ function getFormValues(){
 
 function displayLibrary () {
   removeDivs();
+  removeModal();
   myLibrary.forEach(function (item) {
     let libraryBook = document.createElement('div');
     let libraryBookTitle = document.createElement('div');
@@ -78,4 +77,8 @@ function removeDivs() {
   while (libraryContainer.lastElementChild) {
     libraryContainer.removeChild(libraryContainer.lastElementChild);
   }
+}
+
+function removeModal () {
+  modal.style.display = "none";
 }

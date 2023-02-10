b3b3e8c8d6f68af9b8c82c6a2845faf44bf5d5d5
Nick Sheward <nickshwrd@gmail.com>
2023-02-10 10:46:48 +0000

    Further tidying up of code

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const libraryContainer = document.getElementById('libraryContainer');
 const modal = document.getElementById('newBookModal');
 const modalButton = document.getElementById('modalButton');
-const span = document.getElementsByClassName("close")[0];
-const submitButton = document.getElementById('submitButton');
+const span = document.getElementsByClassName('close')[0];
 const body = document.getElementById('body');
-let myLibrary = [];
+const myLibrary = [];
 
-modalButton.addEventListener("click", () => {
-  modal.style.display ="block";
-  body.style.backgroundColor = "rgba (0,0,0,0.4)";
-  
+// Modal event listeners
+modalButton.addEventListener('click', () => {
+  modal.style.display = 'block';
+  body.style.backgroundColor = 'rgba (0,0,0,0.4)';
 });
-span.addEventListener("click", () => {
-  modal.style.display = "none";
+span.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
-
 
 function Book(title, author, pages, haveRead) {
   this.title = title;
@@ -23,49 +21,60 @@ function Book(title, author, pages, haveRead) {
   this.haveRead = haveRead;
 }
 
-function addBookToLibrary(event) {
-  getFormValues();
-  let newBook = new Book(titleValue, authorValue, pagesValue, haveReadValue);
-  myLibrary.push(newBook);
-  displayLibrary();
-  resetForm();
-  event.preventDefault();
+function removeDivs() {
+  while (libraryContainer.lastElementChild) {
+    libraryContainer.removeChild(libraryContainer.lastElementChild);
+  }
 }
 
-function getFormValues(){
-  titleValue = document.getElementById('title').value;
-  authorValue = document.getElementById('author').value;
-  pagesValue = document.getElementById('pages').value;
-  haveReadValue = document.getElementById('haveRead').checked;
+function hideModal() {
+  modal.style.display = 'none';
 }
 
-function displayLibrary () {
-  removeDivs();
-  hideModal();
-  createLibraryCard();
+function removeButtonEventListener(removeBookButton) {
+  removeBookButton.addEventListener('click', (e) => {
+    const element = document.getElementById(e.target.id);
+    element.remove();
+    delete myLibrary[e.target.id];
+  });
 }
 
-function createLibraryCard () {
-  myLibrary.forEach(function (item, index) {
+function haveReadButtonEventListener(haveReadButton) {
+  haveReadButton.addEventListener('click', (e) => {
+    const haveReadButtonValue = document.getElementById(e.target.id).textContent;
+    const myLibraryArrayIndex = e.target.id.replace('haveRead', '');
+    if (haveReadButtonValue == 'Read') {
+      haveReadButton.style.backgroundColor = 'red';
+      haveReadButton.innerText = 'Not read';
+      myLibrary[myLibraryArrayIndex].haveRead = false;
+    } else if (haveReadButtonValue == 'Not read') {
+      haveReadButton.style.backgroundColor = 'rgb(134, 228, 134)';
+      haveReadButton.innerText = 'Read';
+      myLibrary[myLibraryArrayIndex].haveRead = true;
+    }
+  });
+}
 
-    // Create elements to be appended onto the library cards 
-    let libraryBook = document.createElement('div');
-    let libraryBookTitle = document.createElement('div');
-    let libraryBookAuthor = document.createElement('div');
-    let libraryBookPages = document.createElement('div');
-    let removeBookButton = document.createElement('button');
-    let haveReadButton = document.createElement('button');
+function createLibraryCard() {
+  myLibrary.forEach((item, index) => {
+    // Create elements to append onto the library card
+    const libraryBook = document.createElement('div');
+    const libraryBookTitle = document.createElement('div');
+    const libraryBookAuthor = document.createElement('div');
+    const libraryBookPages = document.createElement('div');
+    const removeBookButton = document.createElement('button');
+    const haveReadButton = document.createElement('button');
     libraryBook.className = 'myLibraryCards';
     libraryBook.id = index;
-    libraryBookValue = item;
     removeBookButton.id = index;
     haveReadButton.id = `haveRead${index}`;
 
-    // Set the content of the elements 
-    libraryBookTitle.innerText = "Title: " +libraryBookValue.title;
-    libraryBookAuthor.innerText = "Author: " +libraryBookValue.author;
-    libraryBookPages.innerText = "Number of pages: " +libraryBookValue.pages;
-    removeBookButton.innerText = "Remove book"
+    // Create content for the elements
+    const libraryBookValue = item;
+    libraryBookTitle.innerText = `Title: ${libraryBookValue.title}`;
+    libraryBookAuthor.innerText = `Author: ${libraryBookValue.author}`;
+    libraryBookPages.innerText = `Number of pages: ${libraryBookValue.pages}`;
+    removeBookButton.innerText = 'Remove book';
     if (libraryBookValue.haveRead == true) {
       haveReadButton.style.backgroundColor = 'rgb(134, 228, 134';
       haveReadButton.innerText = 'Read';
@@ -74,53 +83,39 @@ function createLibraryCard () {
       haveReadButton.innerText = 'Not read';
     }
 
-    // Append elements 
-    libraryBook.append(libraryBookTitle,libraryBookAuthor,libraryBookPages,haveReadButton, removeBookButton);
+    // Append elements
+    libraryBook.append(libraryBookTitle, libraryBookAuthor, libraryBookPages);
+    libraryBook.append(haveReadButton, removeBookButton);
     libraryContainer.appendChild(libraryBook);
 
-    //Add event listeners 
+    // Add event listeners
     removeButtonEventListener(removeBookButton);
-    haveReadButtonEventListener(haveReadButton);  
-})
+    haveReadButtonEventListener(haveReadButton);
+  });
 }
 
-function removeButtonEventListener (removeBookButton) {
-  removeBookButton.addEventListener("click", (e) => {
-    const element = document.getElementById(e.target.id)
-    element.remove();
-    delete myLibrary[e.target.id];
-  }) ;
+function displayLibrary() {
+  removeDivs();
+  hideModal();
+  createLibraryCard();
 }
 
-function haveReadButtonEventListener (haveReadButton) {
-  haveReadButton.addEventListener("click", (e) => {
-    const haveReadButtonValue = document.getElementById(e.target.id).textContent;
-    let myLibraryArrayIndex = e.target.id.replace('haveRead','');
-    if (haveReadButtonValue == 'Read') {
-      haveReadButton.style.backgroundColor = 'red';
-      haveReadButton.innerText = 'Not read';
-      myLibrary[myLibraryArrayIndex].haveRead = false;
-    } else if (haveReadButtonValue == 'Not read') {
-      haveReadButton.style.backgroundColor = 'rgb(134, 228, 134';
-      haveReadButton.innerText = 'Read';
-      myLibrary[myLibraryArrayIndex].haveRead = true;
-    }
-});
-}
-
-function resetForm () {
-  document.getElementById('title').value = "";
-  document.getElementById('author').value = "";
-  document.getElementById('pages').value = "";
+function resetForm() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
   document.getElementById('haveRead').checked = null;
 }
 
-function removeDivs() {
-  while (libraryContainer.lastElementChild) {
-    libraryContainer.removeChild(libraryContainer.lastElementChild);
-  }
-}
-
-function hideModal () {
-  modal.style.display = "none";
+// Function called when form submitted
+function addBookToLibrary(event) {
+  const titleValue = document.getElementById('title').value;
+  const authorValue = document.getElementById('author').value;
+  const pagesValue = document.getElementById('pages').value;
+  const haveReadValue = document.getElementById('haveRead').checked;
+  const newBook = new Book(titleValue, authorValue, pagesValue, haveReadValue);
+  myLibrary.push(newBook);
+  displayLibrary();
+  resetForm();
+  event.preventDefault();
 }

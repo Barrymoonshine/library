7310ef9bbd618391052ee9a2055e4d9fbe44054a
Nick Sheward <nickshwrd@gmail.com>
2023-03-08 08:43:50 +0000

    Display controller functions moved to display class

--- script.js
+++ script.js
@@ -97,50 +97,18 @@ class Library {
     }
   }
 }
-
 const libraryController = new Library(libraryContainer);
 
 class Display {
+  displayModal() {
+    modal.style.display = 'flex';
+    body.style.backgroundColor = 'rgba (0,0,0,0.4)';
+  }
   hideModal() {
     modal.style.display = 'none';
   }
 }
-
 const displayController = new Display();
-
-// Moved to Library Class
-// function deleteBook(e) {
-//   // Deletes the selected book from the array and returns a new array
-//   const element = document.getElementById(e.target.id);
-//   const index = element.id;
-//   myLibrary.splice(index, 1);
-//   displayLibrary();
-// }
-
-// Moved to Library Class
-// function updateReadStatus(e) {
-//   const haveReadButton = document.getElementById(e.target.id);
-//   const myLibraryArrayIndex = e.target.id.replace('haveRead', '');
-//   if (haveReadButton.textContent === 'Read') {
-//     haveReadButton.style.backgroundColor = '#f87171';
-//     haveReadButton.innerText = 'Not read';
-//     myLibrary[myLibraryArrayIndex].haveRead = false;
-//   } else if (haveReadButton.textContent === 'Not read') {
-//     haveReadButton.style.backgroundColor = '#4ade80';
-//     haveReadButton.innerText = 'Read';
-//     myLibrary[myLibraryArrayIndex].haveRead = true;
-//   }
-// }
-
-libraryContainer.addEventListener('click', (e) => {
-  // Checks if the target id only contains numbers and is therefore the delete button
-  if (String(e.target.id).match(/^[0-9]+$/) != null) {
-    libraryController.deleteBook(e);
-    // Else the user has selected the read status button
-  } else {
-    libraryController.updateReadStatus(e);
-  }
-});
 
 function handleForm(e) {
   e.preventDefault();
@@ -152,85 +120,28 @@ function handleForm(e) {
 }
 newBookForm.addEventListener('submit', handleForm);
 
-// Moved to Library class
-// function addBookToLibrary() {
-//   const titleValue = document.getElementById('title').value;
-//   const authorValue = document.getElementById('author').value;
-//   const pagesValue = document.getElementById('pages').value;
-//   const haveReadValue = document.getElementById('have-read').checked;
-//   const newBook = new Book(titleValue, authorValue, pagesValue, haveReadValue);
-//   myLibrary.push(newBook);
-// }
-
-// No longer needed as handled by Library class
-// function displayLibrary() {
-//   removeDivs();
-//   hideModal();
-//   createLibraryCard();
-//   styleHaveReadButton();
-// }
-
-// Moved to Library class
-// function resetForm() {
-//   document.getElementById('title').value = '';
-//   document.getElementById('author').value = '';
-//   document.getElementById('pages').value = '';
-//   document.getElementById('have-read').checked = false;
-// }
-
-// Moved to Library class
-// function removeDivs() {
-//   while (libraryContainer.lastElementChild) {
-//     libraryContainer.removeChild(libraryContainer.lastElementChild);
-//   }
-// }
-
-// Moved to display class
-// function hideModal() {
-//   modal.style.display = 'none';
-// }
-
-// Moved to Library class
-// function createLibraryCard() {
-//   // Creates library card content
-//   myLibrary.forEach((item, index) => {
-//     libraryContainer.innerHTML += String.raw`
-//     <div class='my-library-cards' id='${index}'>
-//       <div>
-//         <p>Title: ${item.title}</p> <br>
-//         <p>Author: ${item.author}</p> <br>
-//         <p>Number of pages: ${item.pages}</p>
-//       </div>
-//       <button id='haveRead${index}'>${item.haveRead}</button>
-//       <button id='${index}'>Remove book</button>
-//     </div>`;
-//   });
-// }
-
-// Moved to Library class
-// function styleHaveReadButton() {
-//   myLibrary.forEach((item, index) => {
-//     const haveReadButton = document.getElementById(`haveRead${index}`);
-//     if (item.haveRead === true) {
-//       haveReadButton.style.backgroundColor = '#4ade80';
-//       haveReadButton.innerText = 'Read';
-//     } else if (item.haveRead === false) {
-//       haveReadButton.style.backgroundColor = '#f87171';
-//       haveReadButton.innerText = 'Not read';
-//     }
-//   });
-// }
+libraryContainer.addEventListener('click', (e) => {
+  // Checks if the target id only contains numbers and is therefore the delete button
+  if (String(e.target.id).match(/^[0-9]+$/) != null) {
+    libraryController.deleteBook(e);
+    // Else the user has selected the read status button
+  } else {
+    libraryController.updateReadStatus(e);
+  }
+});
 
 // Modal event listeners
 modalButton.addEventListener('click', () => {
-  modal.style.display = 'flex';
-  body.style.backgroundColor = 'rgba (0,0,0,0.4)';
+  displayController.displayModal();
 });
+
 closeModalButton.addEventListener('click', () => {
-  modal.style.display = 'none';
+  displayController.hideModal();
 });
+
+// Hides the modal if the user clicks outside of the form
 window.addEventListener('click', (e) => {
   if (e.target === modal) {
-    modal.style.display = 'none';
+    displayController.hideModal();
   }
 });
